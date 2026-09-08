6c7d8a877a0a64c6ec63d5af1ec5b7833a96efdb
sentomk <sentomk@proton.me>
2026-09-08 22:09:38 +0800

    chore: remove leftover incomplete test stub

--- tests/test.cc
+++
@@ -1,9 +0,0 @@
-#include "neko/platforms/elf.hpp"
-#include "neko/session.hpp"
-
-int main() {
-  neko::reload_session session{neko::elf::create_backend()};
-  session.watch("hot.new.o");
-
-  while
-}
